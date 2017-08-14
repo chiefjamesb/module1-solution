3128d17b75971b9fbe6bceeb44ac187e8fa2539e
James Bennett <chiefb@gmail.com>
2017-08-13 17:33:04 -0700

    really

--- app.js
+++ app.js
@@ -8,17 +8,22 @@ MsgController.$inject = ['$scope'];
 function MsgController($scope) {
 
     $scope.value =0;
-    $scope.food ="";
+    $scope.dishes ="";
     $scope.myMessage= "";
 
   $scope.calc = function () {
-    var total = stringSplit($scope.food);
+    var total = stringSplit();
     $scope.value = total;
+    console.log(total);
+    if (total==0)
+    {
+        $scope.myMessage ="Please enter data first";
+    }
     if (total > 3 )
     {
       $scope.myMessage ="Too much!";
     }
-    else
+    if (total > 0 && total <4)
     {
       $scope.myMessage = "Enjoy!";
     }
@@ -27,9 +32,15 @@ function MsgController($scope) {
 
 
   function stringSplit() {
-    var arrayOfStrings = $scope.dishes.split(',');
+    var x =0;
+    console.log($scope.dishes);
+    if ($scope.dishes != "")
+     {
+        var arrayOfStrings = $scope.dishes.split(',');
 
-    var x = arrayOfStrings.length;
+        x = arrayOfStrings.length;
+
+    }
       return x;
   };
 }
